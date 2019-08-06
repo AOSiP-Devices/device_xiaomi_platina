376e7bc6fc9007837631fbc483976b33fd8aed64
Jago Gardiner (nysadev) <nysadev@raphielgang.org>
2019-08-06 22:46:05 +0100

    treewide: jasmine_sprout -> Platina
    Signed-off-by: Jago Gardiner (nysadev) <nysadev@raphielgang.org>

--- power-libperfmgr/Power.cpp
+++ power-libperfmgr/Power.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.power@1.3-service.jasmine_sprout-libperfmgr"
+#define LOG_TAG "android.hardware.power@1.3-service.platina-libperfmgr"
 
 #include <android-base/file.h>
 #include <android-base/logging.h>
